348de01a19f60c3e46b05ad0e39e1078be639845
Shen8Singh <gayashan9090@gmail.com>
2024-10-12 14:28:02 +0530

    creatw blog

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -167,4 +167,15 @@ document.querySelectorAll('.progress').forEach(function(progress) {
 
 //////////////////////////////
 
+$(window).scroll(function() {
+  $('.fadedfx').each(function(){
+  var imagePos = $(this).offset().top;
+
+  var topOfWindow = $(window).scrollTop();
+    if (imagePos < topOfWindow+500) {
+      $(this).addClass("fadeIn");
+    }
+  });
+});
+
 })();
