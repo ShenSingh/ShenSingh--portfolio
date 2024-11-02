ffef56593d1c8207b422ae087d2e27b6191ee1fe
Shen8Singh <gayashan9090@gmail.com>
2024-11-02 20:26:47 +0530

    commit

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -2,33 +2,6 @@
   "use strict";
 
   /**
-   * Easy selector helper function
-   */
-  const select = (el, all = false) => {
-    el = el.trim()
-    if (all) {
-      return [...document.querySelectorAll(el)]
-    } else {
-      return document.querySelector(el)
-    }
-  }
-
-  /**
-   * Easy event listener function
-   */
-  const on = (type, el, listener, all = false) => {
-    let selectEl = select(el, all)
-
-    if (selectEl) {
-      if (all) {
-        selectEl.forEach(e => e.addEventListener(type, listener))
-      } else {
-        selectEl.addEventListener(type, listener)
-      }
-    }
-  }
-
-  /**
    * Scroll to an element with header offset
    */
   const scrollto = (el) => {
@@ -76,4 +49,4 @@ $(window).scroll(function() {
   });
 });
 
-})();
+});
